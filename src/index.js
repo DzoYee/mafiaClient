21f10761ad14675cbd49a51765b645b9695f3227
Joey Leung <joeyleung8@gmail.com>
2017-05-18 22:38:42 -0700

    add room

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Lobby from './components/lobby';
-import Room from './components/room';
+import Room from './room/room';
 
 import reducers from './reducers';
 
